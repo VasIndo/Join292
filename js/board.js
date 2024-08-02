d91f22c691e06f322263aa400bc832b5c48bc348
Christian Porsch <porschchristian1@gmail.com>
2024-08-02 16:50:44 +0200

    added board site

---
+++ js/board.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const taskSections = [
+      { id: 'to-do-plus', hoverSrc: 'assets/img/plus-button-blue.svg', defaultSrc: 'assets/img/plus-button.svg' },
+      { id: 'in-progress-plus', hoverSrc: 'assets/img/plus-button-blue.svg', defaultSrc: 'assets/img/plus-button.svg' },
+      { id: 'await-feedback-plus', hoverSrc: 'assets/img/plus-button-blue.svg', defaultSrc: 'assets/img/plus-button.svg' }
+    ];
+  
+    taskSections.forEach(section => {
+      const element = document.getElementById(section.id);
+  
+      if (element) {
+        element.addEventListener('mouseenter', () => {
+          element.src = section.hoverSrc;
+        });
+  
+        element.addEventListener('mouseleave', () => {
+          element.src = section.defaultSrc;
+        });
+      }
+    });
+  });
--- js/summary.js
+++ js/summary.js
@@ -1,15 +1,22 @@
-function changeToDoIconOnMouseEnter() {
-    document.getElementById('summary-to-do-icon').src = "assets/img/summary-to-do-hover.svg";
-}
-
-function changeToDoIconOnMouseLeave() {
-    document.getElementById('summary-to-do-icon').src = "assets/img/summary-to-do.svg";
-}
-
-function changeDoneIconOnMouseEnter() {
-    document.getElementById('summary-done-icon').src = "assets/img/summary-done-hover.svg";
-}
-
-function changeDoneIconOnMouseLeave() {
-    document.getElementById('summary-done-icon').src = "assets/img/summary-done.svg";
-}
+document.addEventListener("DOMContentLoaded", () => {
+    const icons = [
+      { parentId: 'to-do', iconId: 'summary-to-do-icon', hoverSrc: 'assets/img/summary-to-do-hover.svg', defaultSrc: 'assets/img/summary-to-do.svg' },
+      { parentId: 'done', iconId: 'summary-done-icon', hoverSrc: 'assets/img/summary-done-hover.svg', defaultSrc: 'assets/img/summary-done.svg' }
+    ];
+  
+    icons.forEach(({ parentId, iconId, hoverSrc, defaultSrc }) => {
+      const parentElement = document.getElementById(parentId);
+      const iconElement = document.getElementById(iconId);
+  
+      if (parentElement && iconElement) {
+        parentElement.addEventListener('mouseenter', () => {
+          iconElement.src = hoverSrc;
+        });
+  
+        parentElement.addEventListener('mouseleave', () => {
+          iconElement.src = defaultSrc;
+        });
+      }
+    });
+  });
+  
