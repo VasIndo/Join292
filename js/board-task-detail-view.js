f1cc71b8eaf52a6d281bf0ce1f9c5ef984ece83e
Christian Porsch <porschchristian1@gmail.com>
2024-08-28 21:18:08 +0200

    splitted the board js file

---
+++ js/board-task-detail-view.js
@@ -0,0 +1,8 @@
+/**
+ * Toggles the visibility of the detailed task card view and the main board container.
+ */
+function toggleDetailTaskCard() {
+    document.getElementById("card-detail-view").classList.toggle("show-card");
+    document.getElementById("board-container").classList.toggle("d-none");
+  }
+  
